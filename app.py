d4a2c23de0a9e2d774a828cbc5fd928aea392257
unknown <martamatviyenko@gmail.com>
2025-10-07 10:38:36 +0300

    Changed password

--- app.py
+++ app.py
@@ -5,9 +5,14 @@ from auth.route import user_bp, select_bp, api_bp
 app = Flask(__name__)
 mysql = MySQL(app)
 
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'root'
-app.config['MYSQL_HOST'] = 'localhost'
+# app.config['MYSQL_USER'] = 'root'
+# app.config['MYSQL_PASSWORD'] = 'root'
+# app.config['MYSQL_HOST'] = 'localhost'
+# app.config['MYSQL_DB'] = 'bookingplatform'
+
+app.config['MYSQL_USER'] = 'dfrsrhnzda'
+app.config['MYSQL_PASSWORD'] = 'd3w9Cpaz3Hqq$0gB'
+app.config['MYSQL_HOST'] = 'laboratory-work-server.mysql.database.azure.com'
 app.config['MYSQL_DB'] = 'bookingplatform'
 
 app.config['MYSQL_SSL_DISABLED'] = True
